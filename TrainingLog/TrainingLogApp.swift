fb1dab61d435ea4e51d33937e38f8fcccff9bdcc
Andrew Mitchell <andrew.mac.mitchell@gmail.com>
2026-04-07 11:00:00 -0400

    Initial Commit

---
+++ TrainingLog/TrainingLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingLogApp.swift
+//  TrainingLog
+//
+//  Created by Andrew Mitchell on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TrainingLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
